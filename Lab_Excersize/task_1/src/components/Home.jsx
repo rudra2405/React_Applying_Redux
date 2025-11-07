c3594b5d89d8ad31f60bc36219d97e4159acdd19
rudra2405 <rudrahirdekar4@gmail.com>
2025-11-07 19:54:51 +0530

    changed

--- Lab_Excersize/task_1/src/components/Home.jsx
+++ Lab_Excersize/task_1/src/components/Home.jsx
@@ -1,30 +1,33 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Container } from "react-bootstrap";
+// import { Container } from "react-bootstrap";
+
 const Home = ({ count, increment, decrement }) => {
   return (
-    <Container className="w-50 mx-auto mt-5 p-5">
-      <h1>Counter App via Redux</h1>
-      <hr />
-      <span className="fs-3">The count Initial state value is : {count}</span>
+    <div style={{ maxWidth: "600px", marginLeft: "400px" }}>
+      <div className="p-5 d-flex flex-column align-items-center border border-3 border-dark rounded-3 justify-content-center">
+        <h1>Counter App via Redux</h1>
+        <hr />
+        <span className="fs-3">The count Initial state value is : {count}</span>
 
-      <h2>
-        <button
-          type="button"
-          className="btn btn-sm btn-success fs-1 w-25"
-          onClick={increment}
-        >
-          +
-        </button>
-        <button
-          type="button"
-          className="btn btn-sm btn-danger fs-1 ms-5 w-25"
-          onClick={decrement}
-        >
-          -
-        </button>
-      </h2>
-    </Container>
+        <h2>
+          <button
+            type="button"
+            className="btn btn-sm btn-success fs-1"
+            onClick={increment}
+          >
+            +
+          </button>
+          <button
+            type="button"
+            className="btn btn-sm btn-danger fs-1 ms-5"
+            onClick={decrement}
+          >
+            -
+          </button>
+        </h2>
+      </div>
+    </div>
   );
 };
 
